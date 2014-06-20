214f00f3bb50a8f97f4439cc1923049bb308fd8b
I-Ta Tsai@MBP <ittsai@gmail.com>
2014-06-20 09:26:56 +0800

    use flickr native api to get interestingness photos.

--- flickr_interestingness.rb
+++ flickr_interestingness.rb
@@ -1,5 +1,6 @@
 require 'rss'
 require 'yaml'
+require 'json'
 require 'open-uri'
 
 # read config file
@@ -7,9 +8,11 @@ CONFIG_PATH = './config.yml'
 cfg = YAML.load_file(CONFIG_PATH)
 WORKING_DIR = ( cfg['working_dir'] && Dir.exists?(cfg['working_dir']) ? cfg['working_dir'] : Dir.pwd )
 
-#CONSUMER_KEY = cfg['500px']['key']
-#PHOTOS_PER_PAGE = cfg['500px']['photos_per_page']
-RSS_URL = cfg['default']['flickr']['rss']['url']
+api_fi_url = "https://api.flickr.com/services/rest/?" +
+             "method=flickr.interestingness.getList&" +
+             "format=json&" +
+             "nojsoncallback=1&" +
+             "api_key=" + cfg['flickr']['key']
 
 # remove current images
 ## flickr photos contain '_'
@@ -18,16 +21,28 @@ Dir[WORKING_DIR + "/*.jpg"].select {|x| x =~ /_/}.each do |f|
 end
 
 # read RSS and fetch images
-open(RSS_URL) do |rss|
-  feed = RSS::Parser.parse(rss)
-  feed.items.each do |item|
-    filename = item.link.split('/')[-1]
-    file_path = WORKING_DIR + "/" + filename
+#open(RSS_URL) do |rss|
+rsp = JSON.load(open(api_fi_url))
+if rsp['stat'] == 'ok'
+  rsp['photos']['photo'].each do |photo|
+    api_psize_url = "https://api.flickr.com/services/rest/?" +
+                    "method=flickr.photos.getSizes&" +
+                    "format=json&" +
+                    "nojsoncallback=1&" +
+                    "api_key=" + cfg['flickr']['key'] + "&" +
+                    "photo_id=" + photo['id']
+    rsp_sizes = JSON.load(open(api_psize_url))
+    if rsp_sizes['stat'] == 'ok'
+      largest = rsp_sizes['sizes']['size'][-1]
+      if [largest['width'].to_i, largest['height'].to_i].max >= 1920
+        filename = largest['source'].split('/')[-1]
+        file_path = WORKING_DIR + "/" + filename
 
-    # fetch image and write
-    image = open(item.link).read
-    File.open(file_path, 'w') do |f|
-      f.write image
+        # fetch image and write
+        File.open(file_path, 'w') do |f|
+          f.write open(largest['source']).read
+        end
+      end
     end
   end
 end
